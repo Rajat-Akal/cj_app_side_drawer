1616d69100933ba7dd6d1f2ca2be40f25f5b8c39
Rajat Dash <rajat.dash@akalinfosys.com>
2022-09-26 16:19:28 +0530

    bottom bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_home_page_drawer/home.dart';
+import 'package:flutter_home_page_drawer/main._screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    // home: HomeScreen(),
+    home: Main_Screen(),
   ));
 }
